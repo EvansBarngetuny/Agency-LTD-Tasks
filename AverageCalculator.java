7fa293e7a929b479d6677c002a284c3f1f72a4fb
Evans Kibitok Barngetuny <48053493+EvansBarngetuny@users.noreply.github.com>
2023-06-22 12:38:21 +0300

    Add files via upload

---
+++ AverageCalculator.java
@@ -0,0 +1,16 @@
+public class AverageCalculator {
+    public static double calculateAverage(int[] numbers) {
+        double total = 0;  // Change the data type to double
+        for (int num : numbers) {
+            total += num;
+        }
+        double average = total / numbers.length;
+        return average;
+    }
+
+    public static void main(String[] args) {
+        int[] data = {5, 10, 15, 20, 25};
+        double result = calculateAverage(data);
+        System.out.println("The average is: " + result);
+    }
+}
